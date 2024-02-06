051792c2372629ca0f355a723632c0c0de787b22
Shayxon <shayxon1@gmail.com>
2024-02-06 21:35:56 +0500

    homework

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product, Video
 
-# Register your models here.
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'image']
+    list_filter = ['price']
+    search_fields = ['name']
+
+@admin.register(Video)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['title', 'video']
+    search_fields = ['title']    
--- shopapp/models.py
+++ shopapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Video(models.Model):
+    title = models.CharField(max_length=50)
+    video = models.FileField(upload_to='videos/')
+
+    def __str__(self) -> str:
+        return self.title  
