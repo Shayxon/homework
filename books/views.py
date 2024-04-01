268bd31a2ef1f112bc29ce42b57166a859a31a6b
Shayxon <shayxon1@gmail.com>
2024-04-01 13:18:15 +0500

    homework

--- books/views.py
+++ books/views.py
@@ -5,20 +5,6 @@ from .serializers import ItemSerializer
 from rest_framework import serializers
 from rest_framework import status
 from django.shortcuts import get_object_or_404
- 
-@api_view(['GET'])
-def ApiOverview(request):
-    api_urls = {
-        'all_items': '/',
-        'Search by Category': '/?category=category_name',
-        'Search by Subcategory': '/?subcategory=category_name',
-        'Add': '/create',
-        'Update': '/update/pk',
-        'Delete': '/item/pk/delete'
-    }
- 
-    return Response(api_urls)
-
 
 @api_view(['POST'])
 def add_items(request):
